b6116936619ced54792f4836c8090dfb01e973a0
Ales <ales.brelih@gmail.com>
2017-01-07 14:23:23 +0100

    Created person class

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
+#include "headers/classes.h"
+#include <string>
+
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    Person marjan;
+    Person ales("Ales","Brelih",27,"krneki07@gmail.com");
+    cout<<"Number of persons: "<<Person::GetNumberOfPersons()<<". "<<endl;
+    marjan.Introduce();
+    ales.Introduce();
     return 0;
 }
