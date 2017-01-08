6f6f47a888fcd644c9540e33801b817576640a44
Ales <ales.brelih@gmail.com>
2017-01-08 15:51:35 +0100

    Header with constant values which will be needed in app

---
+++ constants.hpp
@@ -0,0 +1,12 @@
+#ifndef CONSTANTS_HPP_INCLUDED
+#define CONSTANTS_HPP_INCLUDED
+
+#include <string>
+
+class Constants{
+    public:
+        static string GetDbConnection(){return "/home/ales/Databases/sqlite/spaceinvaders/spaceInvaders.db";}
+};
+
+
+#endif // CONSTANTS_HPP_INCLUDED
