ef58bec45f03b2388eca41c9b459211b1d2b9897
Ales <ales.brelih@gmail.com>
2017-01-08 15:52:53 +0100

    Created class for game session in progress

---
+++ game_session.cpp
@@ -0,0 +1,31 @@
+#include "game_session.h"
+
+//constructor for game session
+Game_Session::Game_Session(Person currentplayer){
+
+    //sets current player
+    this->SetPlayer(currentplayer);
+
+};
+
+//destructor for game session
+Game_Session::~Game_Session(){
+    cout<<"Game session destroyed!"<<endl;
+};
+
+//add score function
+void Game_Session::AddScore(int score){
+
+    //pushes score to scores
+    vector<int> scores = this->GetScores();
+    scores.push_back(score);
+
+};
+
+//save score to database
+void Game_Session::SaveScore(int score){
+
+    //TODO: SAVE SCORE TO DB
+
+};
+
---
+++ game_session.h
@@ -0,0 +1,39 @@
+#ifndef GAME_SESSION_H_INCLUDED
+#define GAME_SESSION_H_INCLUDED
+
+#include "headers/classes.h"
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+class Game_Session{
+
+    private:
+        Person player;
+        vector<int> scores;
+    public:
+        //setter for player
+        void SetPlayer(Person current){player = current;}
+
+        //getter for player
+        Person GetPlayer(){return player;}
+
+        //get scores vector
+        vector<int> GetScores(){return scores;}
+
+        //constructor
+        Game_Session(Person player);
+
+        //destructor
+        ~Game_Session();
+
+        //adds score to current session scores
+        void AddScore(int score);
+
+        //saves score to db
+        void SaveScore(int score);
+};
+
+
+#endif // GAME_SESSION_H_INCLUDED
