3695aea3ef4ffd12abd2a5b3ed7c052417e87067
Aleš Brelih <ales.brelih@gmail.com>
2017-01-15 20:01:35 +0100

    Update ShipMissilesController.cpp

--- controllers/ShipMissilesController.cpp
+++ controllers/ShipMissilesController.cpp
@@ -129,7 +129,7 @@ void ShipMissilesController::MoveMissiles(bool &gameRunning){
                 hitAlien.push_back(*missile);
 
             }
-            else{ //redundant to check if border hit once alien was hit -> better performance
+            else{ //redundant to check if border hit once alien was hit
 
                 //check if border hit
                 int borderHit = missile->CheckIfHitBorder();
